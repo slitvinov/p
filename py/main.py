0f8878d78705a77721de7f797edb1e2ae22938ab
Sergey Litvinov <slitvinov@gmail.com>
2019-10-26 19:31:03 +0000

    minor

--- py/main.py
+++ py/main.py
@@ -3,8 +3,32 @@
 import sys
 import requests
 from math import pi
-#u = 'http://192.168.1.11:5000/api/Rover05'
-u = 'http://192.168.1.11:5000/api/elcaduck'
+u = 'http://192.168.1.10:5000/api/Rover05'
+#u = 'http://192.168.1.11:5000/api/elcaduck'
+
+L = 10
+WHITE, GREY, BLACK = 0, 1, 2
+g_n = 10
+g_step = 0.1
+g = [ ]
+for i in range(g_n):
+    g.append(g_n * [WHITE])
+g_spacing = 2.0*L/g_n
+
+def xy2grid(x, y):
+    n = g_n
+    x = n*x/(2.0*L) + n/2.0
+    y = n*y/(2.0*L) + n/2.0
+    x = int(x)
+    y = int(y)
+    if x < 0 or x >= n:
+        return None, None
+    if y < 0 or y >= n:
+        return None, None
+    return int(x), int(y)
+
+def g_update(x, y, ang, lat):
+    print(ang, lat)
 
 def msg(s):
     sys.stderr.write(s)
@@ -40,7 +64,7 @@ def rotate(duration, direction, power):
         return 1
     s = u + '/' + 'rotate'
     p = {'duration': duration, 'direction': direction, 'power': power}
-    r = requests.get(s)
+    r = requests.get(s, p)
     return 0 if r.status_code == 200 else 1
 
 def stop():
@@ -71,6 +95,10 @@ def lidar():
     r = r['intensities']
     return i, r
 
-inc, inten = lidar()
-print(inc)
-print(inten)
+x, y = 0, 0
+print(gps())
+rotate(1, 'right', 1)
+#g_update(x, y, ang)
+#print(inc)
+#print(inten)
+#print(g)
